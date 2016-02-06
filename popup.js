5c0a129ee79166ad86e8a97e72a4c5ed69d2b258
Raúl Gerardo Vázquez Gonzále <rvazquez@raptor.local>
2016-02-05 23:59:16 -0800

    binding de horas extraidas desde el tp hacia la tabla

--- popup.js
+++ popup.js
@@ -4,7 +4,32 @@ function renderStatus(statusText) {
 
 // Update the relevant fields with the new data
 function setDOMInfo(info) {
-  renderStatus(info);
+  var days = ['Lunes', 'Martes','Miercoles','Jueves', 'Viernes', 'Sabado','Domingo'];
+  for (var i = 0; i < 7; i++) {
+    var pto = 0, holiday = 0, worked = 0;
+
+    var day = info.storiesOrBugs[i];
+    for (var tpnumber in day) {
+      if (day.hasOwnProperty(tpnumber)) {
+        if (1117 == tpnumber) {
+          pto += day[tpnumber];
+        } else if (2486 == tpnumber) {
+          holiday += day[tpnumber];
+        } else{
+          worked += day[tpnumber];
+        };
+      }
+    }
+    day["pto"] = pto;
+    day["holiday"] = holiday;
+    day["worked"] = worked;
+
+    $('.'+days[i]+'.PTO').val(pto);
+    $('.'+days[i]+'.Holiday').val(holiday);
+    $('.'+days[i]+'.Laborado').val(worked);
+  };
+
+  renderStatus("Informacion cargada desde TP");
 }
 
 // Once the DOM is ready...
@@ -16,8 +41,8 @@ window.addEventListener('DOMContentLoaded', function () {
   }, function (tabs) {
     // ...and send a request for the DOM info...
     chrome.tabs.sendMessage(
-        tabs[0].id,
-        {from: 'popup', subject: 'DOMInfo'},
+      tabs[0].id,
+      {from: 'popup', subject: 'DOMInfo'},
         // ...also specifying a callback to be called 
         //    from the receiving end (content script)
         setDOMInfo);
